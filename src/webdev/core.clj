92d781321b5f13b0a226c08fae7157a2e3627a8f
Oliver Caldwell <olliec87@gmail.com>
2015-05-16 12:04:06 +0100

    Simple compojure

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -1,19 +1,26 @@
 (ns webdev.core
-  (require [ring.adapter.jetty :as jetty]))
+  (require [ring.adapter.jetty :as jetty]
+           [ring.middleware.reload :refer [wrap-reload]]
+           [compojure.core :refer :all]
+           [compojure.route :as route]))
 
 (defn greet
   "Greets stuff"
   [request]
-  (if (= "/" (:uri request))
-    {:status 200
-     :body "Hello, World!"
-     :headers {}}
-    {:status 404
-     :body "Not found"
-     :headers {}}))
+  {:status 200
+   :body "Hello, World!"})
+
+(defroutes app
+  (GET "/" [] greet))
 
 (defn -main
   "Simple Ring / Jetty server"
   [port-number]
-  (jetty/run-jetty greet
-    {:port (Integer. port-number)}))
+  (jetty/run-jetty app
+                   {:port (Integer. port-number)}))
+
+(defn -dev-main
+  "Dev server, reloads"
+  [port-number]
+  (jetty/run-jetty (wrap-reload #'app)
+                   {:port (Integer. port-number)}))
