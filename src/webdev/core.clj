735d44f85d580ba61ff2e40f3f7572c5d0b76b79
Oliver Caldwell <olliec87@gmail.com>
2015-05-16 11:50:25 +0100

    Added a 404

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -1,11 +1,19 @@
 (ns webdev.core
   (require [ring.adapter.jetty :as jetty]))
 
+(defn greet
+  "Greets stuff"
+  [request]
+  (if (= "/" (:uri request))
+    {:status 200
+     :body "Hello, World!"
+     :headers {}}
+    {:status 404
+     :body "Not found"
+     :headers {}}))
+
 (defn -main
   "Simple Ring / Jetty server"
   [port-number]
-  (jetty/run-jetty
-    (fn [request] {:status 200
-                   :body "Hello, World!"
-                   :headers {}})
+  (jetty/run-jetty greet
     {:port (Integer. port-number)}))
