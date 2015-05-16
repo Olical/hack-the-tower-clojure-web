904fdf8db31a931d0bd346fafc5039b15ab22379
Oliver Caldwell <olliec87@gmail.com>
2015-05-16 13:53:41 +0100

    moar db dependencies

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring "1.4.0-beta2"]
                  [compojure "1.3.4"]
-                 [org.clojure/java.jdbc "0.3.6"]]
+                 [org.clojure/java.jdbc "0.3.6"]
+                 [postgresql/postgresql "9.1-901-1.jdbc4"]]
   :min-lein-version "2.0.0"
   :uberjar-name "webdev.jar"
   :main webdev.core)
