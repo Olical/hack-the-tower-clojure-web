4cc5b383fae72153b4c532d5630fa1de9299fb3f
Oliver Caldwell <olliec87@gmail.com>
2015-05-16 11:27:40 +0100

    "Hello, World!" web server

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -1,6 +1,11 @@
-(ns webdev.core)
+(ns webdev.core
+  (require [ring.adapter.jetty :as jetty]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  "Simple Ring / Jetty server"
+  [port-number]
+  (jetty/run-jetty
+    (fn [request] {:status 200
+                   :body "Hello, World!"
+                   :headers {}})
+    {:port (Integer. port-number)}))
